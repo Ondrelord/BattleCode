c4e820ad5f88b69f942a93d10c263f2755201001
Banzi <kocur.jan@seznam.cz>
2019-03-07 09:25:07 +0100

    Broadcast manager.

---
+++ battlecode-scaffold-2017-master/src/examplefuncsplayer/BroadcastManager.java
@@ -0,0 +1,14 @@
+package examplefuncsplayer;
+
+public class BroadcastManager {
+
+	public static final int SOLDIER_FIELDS_START = 501;
+	
+	public static final int SOLDIER_FIELDS_END = 600;
+	
+	public static final int SOLDIER_ADVERTISING_FIELD = 500;
+	
+	public static final int SOLDIER_TARGETING_START = 601;
+	
+	public static final int SOLDIER_TARGETING_END = 700;
+}
