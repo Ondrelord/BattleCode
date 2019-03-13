48fb53cace8dafc04013e7ff604f205903201d17
Ondrej Čakloš <caklos.ondrej@gmail.com>
2019-03-13 19:33:47 +0100

    scout

--- battlecode-scaffold-2017-master/src/examplefuncsplayer/Scout.java
+++ battlecode-scaffold-2017-master/src/examplefuncsplayer/Scout.java
@@ -1,18 +1,23 @@
 package examplefuncsplayer;
 
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
+import battlecode.common.TreeInfo;
 
 public class Scout extends Robot
 {
 	private MapLocation myLocation;
+	
+	private int state;
 
 	public Scout(RobotController rc) 
 	{
 		super(rc);
+		state = 0;
 	}
 
 	@Override
@@ -36,14 +41,13 @@ public class Scout extends Robot
 							switch(robot.getType())
 							{
 							case GARDENER:
-								System.out.println("Found enemy Gardener");
-								if(!rc.hasMoved())
-									tryMove(myLocation.directionTo(robot.getLocation()));
-								if(myLocation.distanceTo(robot.getLocation()) < 4)
+								//if(!rc.hasMoved())
+									//tryMove(myLocation.directionTo(robot.getLocation()));
+								/*if(myLocation.distanceTo(robot.getLocation()) < 4)
 								{
 									System.out.println("Shooting");
 									if(rc.canFireSingleShot()) rc.fireSingleShot(myLocation.directionTo(robot.getLocation()));
-								}
+								}*/
 								break;
 							case ARCHON:
 								rc.broadcastFloat(BroadcastType.EnemyArchonLocationX.getChannel(), robot.getLocation().x);
@@ -55,15 +59,59 @@ public class Scout extends Robot
 						}
 					}
 				}
-				if(!rc.hasMoved())
-					tryMove(myLocation.directionTo(enemyLocation));
+				//if(!rc.hasMoved())
+					//tryMove(myLocation.directionTo(enemyLocation));
+				for (TreeInfo tree : rc.senseNearbyTrees(2))
+				{
+					if(rc.canShake(tree.ID))
+					{
+						rc.shake(tree.ID);
+						System.out.println("Tree Shaken");
+					}
+				}
+				
+				scoutMap();
+				
 			}
 			catch (Exception e) 
 			{
 				e.printStackTrace();
 			}
 		}
-		
 	}
 
+	public void scoutMap()
+	{
+		try 
+		{
+			if (!rc.hasMoved()) 
+			{
+				
+				switch (state)
+				{
+				case 0:
+					if(!tryMove(Direction.EAST))
+						state = 1;
+					break;
+				case 1:
+					if(!tryMove(Direction.NORTH))
+						state = 2;
+					break;
+				case 2:
+					if(!tryMove(Direction.WEST))
+						state = 3;
+					break;
+				case 3:
+					if(!tryMove(Direction.SOUTH))
+						state = 0;
+					break;
+				}
+			}
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
 }
